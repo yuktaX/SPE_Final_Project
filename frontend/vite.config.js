ee97d5370dffa5ce6f7a3dec6cfd9bbb0e7f0cc6
yuktaX <yukta.rajapur@gmail.com>
2024-11-20 22:03:19 +0530

    fixed frontend files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Port for development server
+  },
+  build: {
+    outDir: "dist", // Directory for production build
+  },
+});
