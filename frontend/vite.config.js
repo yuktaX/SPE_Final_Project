1c9f98d74b61ebc001c0429906f0d68bf8c579c6
yuktaX <yukta.rajapur@gmail.com>
2024-11-28 14:12:43 +0530

    i hopefully fixed the host issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Port for development server
+    host: true, // Bind to all network interfaces (0.0.0.0)
   },
   build: {
     outDir: "dist", // Directory for production build
