6e7f5f24f3707c5cd0ce428bf307f21f3227fbe1
yuktaX <yukta.rajapur@gmail.com>
2024-12-08 20:01:47 +0530

    fixing merge conflict

--- frontend/src/Layout/ChatPage2.jsx
+++ frontend/src/Layout/ChatPage2.jsx
@@ -98,8 +98,7 @@ export const ChatPage2 = () => {
   };
 
   const connect = () => {
-    //let sock = new SockJS("http://localhost:8081/ws");
-    let sock = new SockJS("http://172.17.0.2:31123/ws");
+    let sock = new SockJS("http://192.168.49.2:30008/ws");
     stompClient = over(sock);
     stompClient.connect({}, onConnect, onError);
   };
